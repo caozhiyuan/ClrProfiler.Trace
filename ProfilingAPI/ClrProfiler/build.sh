48e8d0bcb4aee883272959edaed62223ad7f440b
caozhiyuan <568022847@qq.com>
2019-01-27 20:29:38 +0800

    cmake build

--- ProfilingAPI/ClrProfiler/build.sh
+++ ProfilingAPI/ClrProfiler/build.sh
@@ -4,7 +4,7 @@
 [ -z "${BuildOS:-}"      ] && BuildOS=Linux
 [ -z "${BuildArch:-}"    ] && BuildArch=x64
 [ -z "${BuildType:-}"    ] && BuildType=Debug
-[ -z "${Output:-}"       ] && Output=CorProfiler.so
+[ -z "${Output:-}"       ] && Output=ClrProfiler.so
 
 printf '  CORECLR_PATH : %s\n' "$CORECLR_PATH"
 printf '  BuildOS      : %s\n' "$BuildOS"
@@ -16,8 +16,8 @@ printf '  Building %s ... ' "$Output"
 CXX_FLAGS="$CXX_FLAGS -Wno-invalid-noreturn -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++11"
 INCLUDES="-I $CORECLR_PATH/src/pal/inc/rt -I $CORECLR_PATH/src/pal/prebuilt/inc -I $CORECLR_PATH/src/pal/inc -I $CORECLR_PATH/src/inc -I $CORECLR_PATH/bin/Product/$BuildOS.$BuildArch.$BuildType/inc"
 
-mkdir -p obj/$BuildType/$BuildArch
+mkdir -p build
 
-clang++-3.9 -shared -o obj/$BuildType/$BuildArch/$Output $CXX_FLAGS $INCLUDES miniutf.cpp string.cpp util.cpp logging.cpp config_loader.cpp il_rewriter.cpp il_rewriter_wrapper.cpp clr_helpers.cpp CorProfiler.cpp ClassFactory.cpp dllmain.cpp
+clang++-3.9 -shared -o build/$Output $CXX_FLAGS $INCLUDES miniutf.cpp string.cpp util.cpp logging.cpp config_loader.cpp il_rewriter.cpp il_rewriter_wrapper.cpp clr_helpers.cpp CorProfiler.cpp ClassFactory.cpp dllmain.cpp
 
 printf 'Done.\n'
